efb83007c51c7ebe2a24755476f9a7a249039842
Vini-Nice <viniciussantanapsn@gmail.com>
2025-02-12 16:36:53 -0300

    Aula1

---
+++ aula1/atividade.js
@@ -0,0 +1,24 @@
+// Média
+
+let nota1= 5;
+let nota2=8;
+let media= (nota1+nota2)/2;
+console.log(`A media das notas ${nota1} e ${nota2} é ${media}`);
+
+// IMC
+let idade= 18;
+let peso= 80;
+let altura= 1.80;
+let imc= peso/(altura*altura);
+console.log(`Seu IMC e ${imc}`);
+
+// Temperatura
+let Celsius= 20;
+let Fahrenheit= (Celsius*9/5)+32;
+console.log(`A temperatura em Fahrenheit e ${Fahrenheit}`);
+
+// Desconto
+let preco= 10;
+let desconto= 0.15;
+let precoFinal= preco*(1-desconto);
+console.log(`O preco final com o desconto de ${desconto} ficou em ${precoFinal}`);
---
+++ aula1/teste.js
@@ -0,0 +1,21 @@
+// const nome= "tux";
+// const dataNascimento= 2000;
+
+// console.log(`Idade: ${2025 - dataNascimento}`);
+// console.log(typeof dataNascimento);
+
+// console.log(`Seu nome é ${nome}.`);
+// console.log(typeof nome);
+
+// const altura= 1.80;
+// console.log(`Altura: ${altura}`);
+// console.log(typeof altura);
+
+{
+    var nome= "tux";
+    var Idade= 18;
+    console.log("Nome:", nome);
+    console.log(`Idade:, ${Idade}`);
+}
+console.log(`Nome:, ${nome}`);
+console.log(`Idade:, ${Idade}`);
